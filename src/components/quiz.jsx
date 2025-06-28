f5af54ad0a510d16e8dddc7d004f2ce82cc788b9
quentin kinard <quentinskinard@gmail.com>
2025-06-28 08:57:27 -0500

    removed questionBank code block

--- src/components/quiz.jsx
+++ src/components/quiz.jsx
@@ -39,29 +39,6 @@ function Quiz() {
     return txt.value;
   }
 
-  const questionBank = [
-    {
-      question: "What is the capital of France?",
-      options: ["Berlin", "London", "Paris", "Rome"],
-      answer: "Paris",
-    },
-    {
-      question: "What language is used for web apps?",
-      options: ["PHP", "Python", "JavaScript", "All"],
-      answer: "All",
-    },
-    {
-      question: "What does JSX stand for?",
-      options: [
-        "JavaScript XML",
-        "Java Syntax eXtension",
-        "Just a Simple eXample",
-        "None of the above",
-      ],
-      answer: "JavaScript XML",
-    },
-  ];
-
   function handleSelectOption(option) {
     const newUserAnswers = [...usersAnswers];
     newUserAnswers[currentQuestion] = option;
