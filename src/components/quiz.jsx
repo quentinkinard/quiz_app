e55354461c64a51730014af66f9c22683dc2304e
quentin kinard <quentinskinard@gmail.com>
2025-06-28 08:54:48 -0500

    added a quiz api to render instead of hard coded questions

--- src/components/quiz.jsx
+++ src/components/quiz.jsx
@@ -1,13 +1,43 @@
 import React, { useState } from "react";
 import Results from "../components/results";
+import axios from "axios";
 
 function Quiz() {
   const initialAnswers = [null, null, null];
   const [usersAnswers, setUsersAnswers] = useState(initialAnswers);
+  const [questions, setQuestions] = useState([]);
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [isQuizFinished, setIsQuizFinished] = useState(false);
 
   const selectedAnswer = usersAnswers[currentQuestion];
+
+  const getQuestions = () => {
+    axios
+      .get("https://opentdb.com/api.php?amount=10&type=multiple")
+      .then((response) => {
+        console.log(response.data.results);
+        const cleaned = response.data.results.map((q) => {
+          const options = [...q.incorrect_answers];
+          options.splice(Math.floor(Math.random() * 4), 0, q.correct_answer);
+          return {
+            question: decodeHTML(q.question),
+            options: options.map(decodeHTML),
+            answer: decodeHTML(q.correct_answer),
+          };
+        });
+
+        setQuestions(cleaned);
+        setUsersAnswers(Array(cleaned.length).fill(null));
+        setCurrentQuestion(0);
+      })
+      .catch((err) => console.error(err));
+  };
+
+  function decodeHTML(html) {
+    const txt = document.createElement("textarea");
+    txt.innerHTML = html;
+    return txt.value;
+  }
 
   const questionBank = [
     {
@@ -35,12 +65,11 @@ function Quiz() {
   function handleSelectOption(option) {
     const newUserAnswers = [...usersAnswers];
     newUserAnswers[currentQuestion] = option;
-
     setUsersAnswers(newUserAnswers);
   }
 
   function goToNext() {
-    if (currentQuestion === questionBank.length - 1) {
+    if (currentQuestion === questions.length - 1) {
       setIsQuizFinished(true);
     } else {
       setCurrentQuestion(currentQuestion + 1);
@@ -54,37 +83,50 @@ function Quiz() {
   if (isQuizFinished) {
     return (
       <Results
+        questions={questions}
         userAnswers={usersAnswers}
-        questionBank={questionBank}
         restartQuiz={restartQuiz}
       />
     );
   }
   function restartQuiz() {
-    setUsersAnswers(initialAnswers);
+    setUsersAnswers([]);
     setCurrentQuestion(0);
     setIsQuizFinished(false);
+  }
+
+  if (questions.length === 0) {
+    return (
+      <button className="option" onClick={getQuestions}>
+        Start Quiz
+      </button>
+    );
   }
   return (
     <div>
       <h2>Question {currentQuestion + 1}</h2>
-      <p className="question">{questionBank[currentQuestion].question}</p>
+      {questions.length > 0 && (
+        <p className="question">{questions[currentQuestion].question}</p>
+      )}
 
-      {questionBank[currentQuestion].options.map((option) => (
-        <button
-          className={`option ${selectedAnswer === option ? "selected" : ""}`}
-          onClick={() => handleSelectOption(option)}
-        >
-          {option}
-        </button>
-      ))}
+      {questions.length > 0 &&
+        questions[currentQuestion] &&
+        questions[currentQuestion].options.map((option) => (
+          <button
+            key={option}
+            className={`option ${selectedAnswer === option ? "selected" : ""}`}
+            onClick={() => handleSelectOption(option)}
+          >
+            {option}
+          </button>
+        ))}
 
       <div className="nav-buttons">
         <button onClick={goToPrev} disabled={currentQuestion === 0}>
           Previous
         </button>
         <button onClick={goToNext} disabled={!selectedAnswer}>
-          {currentQuestion === questionBank.length - 1 ? "Finish Quiz" : "Next"}
+          {currentQuestion === questions.length - 1 ? "Finish Quiz" : "Next"}
         </button>
       </div>
     </div>
